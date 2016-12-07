3650cdcfb7e14d2f6f7b578cdfa664431032c002
Amr-Abd-El-Latief <amr@amrs-mbp-2.cairo-sv.eg.ibm.com>
2016-12-07 15:40:27 +0200

    Level 1 and 2 From the Course

---
+++ AngularExamples/web/jsModules/app_1.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+(function(){
+var app = angular.module('store', [ ]); // end of declaring module
+
+app.controller('StoreController',function(){
+    
+   this.product = gem;
+    
+}); //end of the controller
+
+var gem = {
+    
+    name:'Amr',
+    price : 2.25,
+    desc:'...'
+    
+}
+
+
+})()
